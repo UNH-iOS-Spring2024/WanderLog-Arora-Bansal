b03872b463ec33b1f08f0018730b118535412371
Tanushree Arora <aroratanushree.28@gmail.com>
2024-03-22 14:31:37 -0400

    Home page view

---
+++ WanderLog/View/HomePageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageView.swift
+//  WanderLog
+//
+//  Created by Tanushree Arora on 3/22/24.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePageView()
+}
